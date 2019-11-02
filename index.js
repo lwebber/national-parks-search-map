b0ca705d799334a2708ba4129181ef691bb34f6e
Laura Webber <wwebby1@gmail.com>
2019-11-01 19:13:49 -0700

    mapping works

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const apiKey = "qxerkHGC1cOFXxcU0XMPH5FBDsDatsjhbLJby4gd";
 
 const searchURL = `https://developer.nps.gov/api/v1/parks`;
 
-const mymap = L.map('mapid').setView([36, -100], 4);
+const myMap = L.map('mapid').setView([36, -100], 4);
 
 function formatQueryParams(params) {
     const queryItems = Object.keys(params)
@@ -40,23 +40,21 @@ function paintMap() {
         maxZoom: 18,
         id: 'mapbox.streets',
         accessToken: 'pk.eyJ1Ijoid3dlYmJ5MSIsImEiOiJjazI3dWs2dTEwdHFxM2lxaGJndjRpdzZiIn0.rlFiPrSydlJ-HY3K4cdTgw'
-    }).addTo(mymap);
+    }).addTo(myMap);
     $('#mapid').show();
 }
 
 function mapParks(responseJson, max) {
-    console.log('mapParks ran');
     for (let i = 0; i < max; i++) {
         let park = responseJson.data[i];
-
-        let lat = park.latLong.slice(4, 15);
+        let park_full_name = park.fullName;
+        console.log(`name: ${park_full_name}`);
+        let lat = park.latLong.slice(4, 14);
         console.log(`lat: ${lat}`);
         let long = park.latLong.slice(22, 31);
         console.log(`long: ${long}`);
         let park_url = park.url;
-        console.log(`url: ${park_url}`);
-        let park_full_name = park.fullName;
-        console.log(`name: ${park_full_name}`);
+
 
         let parkmarker = L.marker([lat, long]).bindPopup(`<a href="${park_url}" target="_blank">${park_full_name}</a>`);
         parkmarker.addTo(myMap);
